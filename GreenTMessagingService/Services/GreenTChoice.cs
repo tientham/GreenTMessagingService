41b1f82e5a870a05e775f5c6cd3b5ce6f13d9e73
Minh Tien TO (TienTham) <tien.tominh@gmail.com>
2019-01-06 14:25:22 +0100

    adding question and choice

---
+++ GreenTMessagingService/Services/GreenTChoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GreenTMessagingService.Services
+{
+    public class GreenTChoice
+    {
+        public string Title { get; set; }
+
+        public string Question { get; set; }
+
+        public string Destruction { get; set; }
+
+        /// <summary>
+        /// Gets or sets the items displayed in the list.
+        /// </summary>
+        /// <value>The items.</value>
+        public string[] Items { get; set; }
+
+        public Action<string> OnCompleted { get; set; }
+    }
+}
---
+++ GreenTMessagingService/Services/GreenTQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace GreenTMessagingService.Services
+{
+    public class GreenTQuestion
+    {
+        public string Title { get; set; }
+
+        public string Question { get; set; }
+
+        /// <summary>
+        /// Gets or sets the positive button text.
+        /// </summary>
+        /// <value>The positive.</value>
+        public string Positive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the negative button text.
+        /// </summary>
+        /// <value>The negative.</value>
+        public string Negative { get; set; }
+
+        public Action<bool> OnCompleted { get; set; }
+    }
+}
